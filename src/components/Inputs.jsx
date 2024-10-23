de81d3639992ddcab24c8f9bf92458dd5e874354
Ayush Prakash <ayushprakash800@gmail.com.com>
2024-10-23 17:46:18 +0530

    initial commit

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,53 @@
+import { BiCurrentLocation, BiSearch } from "react-icons/bi"
+import { useState } from "react"
+
+const Inputs = ({setQuery, setUnits}) => {
+  const [city,setCity]=useState("");
+  const handleSearchClick=()=>{
+    if(city!=="") setQuery({q:city});
+  };
+  const handleLocationClick=()=>{
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+            const {latitude,longitude} = position.coords
+            setQuery({lat: latitude,lon:longitude})
+        })
+    }
+  }
+    return (
+    <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+            <input 
+            value={city}
+            onChange={(e)=>setCity(e.currentTarget.value)}
+            type="text"
+            placeholder="search by city..."
+            className="text-gray-500 text-xl font-light p-2 w-96 shadow-xl
+            capitalize focus:outline-none placeholder:lowercase" />
+            <BiSearch
+                size={30}
+                className="cursor-pointer transition ease-out hover:scale-125"
+                onClick={handleSearchClick}
+            />
+            <BiCurrentLocation
+                size={30}
+                className="cursor-pointer transition ease-out hover:scale-125"
+                onClick={handleLocationClick}
+            />
+        </div>
+        <div className="flex flex-row w-1/4 items-center justify-center">
+            <button className="text-2xl font-medium transition ease-out hover:scale-125"
+            onClick={()=>setUnits("metric")}>
+                °Celsius    
+            </button> 
+            <p className="text-2xl font-medium mx-5">|</p>
+            <button className="text-2xl font-medium transition ease-out hover:scale-125"
+            onClick={()=>setUnits("imperial")}>
+                °Fahrenheit    
+            </button> 
+        </div>
+    </div>
+  )
+}
+
+export default Inputs
